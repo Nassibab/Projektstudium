765bb1eb6e88cf187dc24fa0168a5552fbdc9c6e
hm-montagel <maria.montagel@web.de>
2026-06-08 02:05:20 +0200

    feat: add basic LLM service integration for moderation
    Implemented a simple LLM API call in the moderation service that can be triggered via a POST request from analyse-r. Added environment variable configuration and updated .gitignore to securely store API Key.

--- services/moderation/app/services/moderate.py
+++ services/moderation/app/services/moderate.py
@@ -1,10 +1,14 @@
-
+from app.services.llm import test_llm
 
 async def moderate(payload: dict):
 
-    print("Kommentar empfangen:")
-    print(payload)
+    print("Kommentar empfangen und LLM testen:")
+    
+    #llm test
+    llm_answer = test_llm()
 
     return {
-        "status": "ok"
+        "status": "ok",
+        "received": payload,
+        "llm_answer": llm_answer,
     }
