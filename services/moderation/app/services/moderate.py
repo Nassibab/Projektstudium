97ab7b7882af06a2e4e0e745f03ef3ac49d78dbc
hm-montagel <maria.montagel@web.de>
2026-06-09 21:04:55 +0200

    Create two reduced test datasets: one containing 30 samples and another with all TF-IDF-based text features removed.  (The original Analyse-R export included a large number of TF-IDF features generated from word-level text analysis, making the dataset too large for  local testing.)

--- services/moderation/app/services/moderate.py
+++ services/moderation/app/services/moderate.py
@@ -1,9 +1,18 @@
 from app.services.llm import test_llm
 
+from app.services.aggregate import analyze_frequency
+
 async def moderate(payload: dict):
 
     print("Kommentar empfangen und LLM testen:")
-    
+
+    #Kommentar kommt an und wird in der Frequenzanalyse analysiert
+    result = analyze_frequency(payload)
+
+    # Für einen bestimmten thread wird eine Warnung herausgegeben
+    # Für die sich darin enthalten Kommentare soll Gegenrede erzeugt werden
+
+
     #llm test
     llm_answer = test_llm()
 
